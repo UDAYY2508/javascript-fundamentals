74c5d7868303308956fa7f6984224198bc97fc15
udayy <udayghorpade2508@gmail.com>
2024-12-27 14:44:15 +0530

    Add files via upload

---
+++ jsbasic/basic.js
@@ -0,0 +1,20 @@
+console.log("hi");
+
+a = 3;
+b = 4;
+c = 13;
+
+d = a+b+c;
+ab = "uday";
+
+console.log(ab);
+let a1 = 19;
+if(a1 >18){
+    console.log("you can drive");
+    
+}
+else{
+    console.log("you cant");
+    
+}
+
---
+++ jsbasic/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function name(n) {
+    console.log("my name is " + n);
+    console.log(n + " is my name");
+    console.log("the " + n + " is a coder");
+}
+name ("uday")
