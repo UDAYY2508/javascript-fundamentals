d2c0f0b66b706f82e14fb563c9f147fa0b467c64
udayy <udayghorpade2508@gmail.com>
2024-12-28 17:27:39 +0530

    Add files via upload

---
+++ jsfun/fun.js
@@ -0,0 +1,18 @@
+/*console.log("function");
+
+function add(a ,b){
+    console.log(a+b);
+    
+}
+add(3,5)
+console.log("uday");
+console.log("uday");
+
+const fun1 = (x)=>{
+    console.log("arrow fun",x);
+    
+}
+fun1(24);
+fun1 (10);
+*/ 
+
