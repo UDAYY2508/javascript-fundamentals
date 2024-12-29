cc1ba02e70040804e32af8b500e6afe783e6063a
udayy <udayghorpade2508@gmail.com>
2024-12-29 18:18:33 +0530

    Add files via upload

---
+++ jsstringmethods/stringmeth.js
@@ -0,0 +1,6 @@
+let name = "bobby"
+console.log(name);
+/*console.log(name.charAt(1));
+console.log(name.indexOf("o"));
+console.log(name.lastIndexOf("b"));*/
+console.log(name.length);
---
+++ jsstringmethods/strmeth2.js
@@ -0,0 +1,7 @@
+let name = "uday ghorpade";
+let fname = name.slice(0,4);
+console.log(fname);
+let lname = name.slice(5);
+console.log(lname);
+let lchar = name.slice(-1);
+console.log(lchar);
