58d85730130155b75527fbc7b26c77f1ae5c14ff
udayy <udayghorpade2508@gmail.com>
2025-01-01 15:42:14 +0530

    Add files via upload

---
+++ jsgo/map.js
@@ -0,0 +1,32 @@
+/*const num = [1, 2, 3,]
+
+const square = num.map(sqr);
+const cubenum = num.map(cube);
+
+function sqr (element){
+    return Math.pow(2, element)
+}
+function cube (element){
+    return Math.pow(3, element)
+}
+
+console.log(cubenum);*/
+
+/*const students = ["uday","raj","bobby","ad"];
+const studnames = students.map(uppercase);
+function uppercase (element){
+    return element.toUpperCase(); 
+}
+
+console.log(studnames);
+
+const date = ["2020-01-10","2021-01-10","2022-01-10",];
+const formtedDate = date.map(datesplit);
+
+function datesplit (element){
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}}`
+
+}
+
+console.log(formtedDate);*/
