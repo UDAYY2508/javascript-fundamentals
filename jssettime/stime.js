dd282f14b95c26d95da9bae4d8afd0488d1c5f19
udayy <udayghorpade2508@gmail.com>
2025-01-25 01:40:37 +0530

    Add files via upload

---
+++ jssettime/stime.js
@@ -0,0 +1,8 @@
+
+/*setTimeout(function(){console.log("hello");},3000);
+const timeOut = setTimeout(()=>console.log("hi udyy"),5000)
+function*/
+
+function startTimer(){
+    setTimeout(()=> window.alert("helooo"),3000)
+}
