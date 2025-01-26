8be8bd17471b9c589cc5afe2729a713e2f99bb20
udayy <udayghorpade2508@gmail.com>
2025-01-26 18:44:20 +0530

    Add files via upload

---
+++ jsmodule/index.js
@@ -0,0 +1,6 @@
+import {pi,getArea} from './module.js';
+
+console.log(pi);
+
+const carea = getArea(5);
+console.log(carea);
---
+++ jsmodule/module.js
@@ -0,0 +1,5 @@
+export const pi = 3.14;
+
+export function getArea(r){
+    return pi * r * r;
+}
